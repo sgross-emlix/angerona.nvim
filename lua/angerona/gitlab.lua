7c8dce5fa1eb7dce9e81cd9a477adbabb6ef16dc
Sebastian Gross <sgross@emlix.com>
2025-03-14 20:01:39 +0100

    draft

---
+++ lua/angerona/gitlab.lua
@@ -0,0 +1,173 @@
+local M = {}
+
+local request
+
+local util = require("angerona.util")
+
+local TRACKER_ID_TASK = 16
+
+M.cfg = nil
+
+M.state = {}
+
+local function get_project_id_from_parent(parent)
+	local response = request.get(parent)
+
+	if response == nil then
+		vim.notify("Failed to get Project ID from: " .. parent, vim.log.levels.ERROR)
+		return nil
+	end
+
+	return response.issue.project.id
+end
+
+function M.read_issue(issue_id)
+	local response = request.get(issue_id)
+
+	if response == nil then
+		vim.notify("Failed to read : " .. issue_id, vim.log.levels.ERROR)
+		return
+	end
+
+	local issue = response.issue
+
+	local lines = {}
+	for s in issue.description:gmatch("([^[\n\r]+)[\n\r]*") do
+		table.insert(lines, s)
+	end
+
+	local buf = util.set_buffer(issue_id)
+	vim.api.nvim_buf_set_lines(buf, 0, -1, true, { issue.subject, "", table.unpack(lines) })
+
+	vim.api.nvim_buf_create_user_command(0, "RedmineCommit",
+		M.update_issue
+		, { force = true }
+	)
+end
+
+function M.update_issue()
+	issue_id = util.get_issue_from_buf_name()
+
+	local buf_subject = vim.api.nvim_buf_get_lines(0, 0, 1, true)[1]
+	local lines = vim.api.nvim_buf_get_lines(0, 2, -1, true)
+
+	local buf_description = ""
+	for _, line in ipairs(lines) do
+		buf_description = buf_description .. "\n" .. line
+	end
+
+	local body = {
+		issue = {
+			subject = buf_subject,
+			description = buf_description,
+		},
+	}
+
+	local response = request.put(issue_id, body)
+
+	if response == nil then
+		vim.notify("Failed to update issue!", vim.log.levels.ERROR)
+		return
+	end
+	vim.notify("Task updated: " .. issue_id, vim.log.levels.INFO)
+end
+
+function M.create_task(project_id, parent_id)
+	local buf_subject = vim.api.nvim_buf_get_lines(0, 0, 1, true)[1]
+	local lines = vim.api.nvim_buf_get_lines(0, 2, -1, true)
+
+	local buf_description = ""
+	for _, line in ipairs(lines) do
+		buf_description = buf_description .. "\n" .. line
+	end
+
+	local body = {
+		issue = {
+			project_id = project_id,
+			subject = buf_subject,
+			description = buf_description,
+			tracker_id = TRACKER_ID_TASK,
+			parent_issue_id = tonumber(parent_id),
+		},
+	}
+
+	local response = request.post(nil, body)
+
+	if response == nil then
+		vim.notify("Failed to create task!", vim.log.levels.ERROR)
+		return
+	end
+
+	vim.notify("Task created: " .. response.issue.id, vim.log.levels.INFO)
+
+	M.state.last_created = response.issue.id
+	vim.api.nvim_buf_set_name(0, util.get_buf_name_from_issue(response.issue.id))
+
+	vim.api.nvim_buf_create_user_command(0, "RedmineCommit",
+		M.update_issue
+		, { force = true }
+	)
+end
+
+function M.open_browser(issue_id)
+	request.open(issue_id)
+end
+
+function M.callback_read(opts)
+	local issue_id = util.get_issue_id(M.state, "Issue", opts.fargs, M.cfg.default_issue)
+
+	if issue_id == "" then
+		vim.notify("Issue ID is required.", vim.log.levels.ERROR)
+		return
+	end
+
+	M.read_issue(issue_id)
+
+	M.state.last = issue_id
+end
+
+function M.callback_create(opts)
+	local parent = util.get_issue_id(M.state, "Parent", opts.fargs, M.cfg.default_issue)
+	if parent == "" then
+		vim.notify("Parent issue ID is required.", vim.log.levels.ERROR)
+		return
+	end
+
+	local project = get_project_id_from_parent(parent)
+	if project == nil then
+		vim.notify("Project ID is required.", vim.log.levels.ERROR)
+		return
+	end
+
+	util.set_buffer()
+
+	vim.api.nvim_buf_create_user_command(0, "RedmineCommit",
+		function()
+			M.create_task(project, parent)
+		end
+		, {}
+	)
+end
+
+function M.callback_open(opts)
+	local issue_id = util.get_issue_id(M.state, "Issue", opts.fargs, M.cfg.default_issue)
+
+	if issue_id == "" then
+		vim.notify("issue_id ID is required.", vim.log.levels.ERROR)
+		return
+	end
+
+	M.open_browser(issue_id)
+
+	M.state.last = issue_id
+end
+
+function M.setup(config)
+	M.cfg = config
+
+	--	request = require("angerona.http").setup(config, "issues")
+
+	return M
+end
+
+return M
--- lua/angerona/init.lua
+++ lua/angerona/init.lua
@@ -5,28 +5,45 @@ table.unpack = table.unpack or unpack
 
 local util = require("angerona.util")
 
+local function setup_backend(backend, config)
+	local class = require("angerona." .. backend)
+
+	require("angerona.debug").dump_object(config)
+
+	if not class then
+		vim.notify("backend" .. backend .. "not implemented")
+		return
+	end
+
+	class.setup(config)
+
+	local name = backend:sub(1, 1):upper() .. backend:sub(2)
+
+	vim.api.nvim_create_user_command(
+		name .. "Read",
+		class.callback_read,
+		{ nargs = "*", desc = "Read " .. name .. " issue via REST API into dedicated buffer" }
+	)
+
+	vim.api.nvim_create_user_command(
+		name .. "Create",
+		class.callback_create,
+		{ nargs = "*", desc = "Create a " .. name .. " task via REST API" }
+	)
+
+	vim.api.nvim_create_user_command(
+		name .. "Open",
+		class.callback_open,
+		{ nargs = "*", desc = "Open current " .. name .. " issue URL in a web browser" }
+	)
+end
+
 function M.setup(plugin_config)
 	M.cfg = util.load_config(plugin_config)
 
-	local redmine = require("angerona.redmine").setup(M.cfg.redmine)
-
-	vim.api.nvim_create_user_command(
-		"RedmineRead",
-		redmine.callback_read,
-		{ nargs = "*", desc = "Read Redmine issue via REST API into dedicated buffer" }
-	)
-
-	vim.api.nvim_create_user_command(
-		"RedmineCreate",
-		redmine.callback_create,
-		{ nargs = "*", desc = "Create a Redmine task via REST API" }
-	)
-
-	vim.api.nvim_create_user_command(
-		"RedmineOpen",
-		redmine.callback_open,
-		{ nargs = "*", desc = "Open current redmine issue URL in a web browser" }
-	)
+	for backend, cfg in pairs(M.cfg.backends) do
+		setup_backend(backend, cfg)
+	end
 end
 
 return M
